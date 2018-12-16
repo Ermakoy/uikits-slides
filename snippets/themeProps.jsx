3213ada96d49802c53406e4ae668dcd483e29917
Ermakoy <own3d.rus@gmail.com>
2018-12-16 22:20:02 +0300

    add small improval

--- snippets/themeProps.jsx
+++ snippets/themeProps.jsx
@@ -1,17 +1,32 @@
-// font-size: 24px (theme.fontSizes[4])
+// width: 50%
+<Box width={1/2} />
+
+// font-size: 20px (theme.fontSizes[4])
 <Box fontSize={4} />
 
-// margin: 16px (theme.space[3])
+// margin: 16px (theme.space[2])
 <Box m={2} />
 
-// color: #333 (theme.colors.blacks[0])
-<Box color="blacks.3" />
+// padding: 32px (theme.space[3])
+<Box p={3} />
 
-// background color (theme.colors['light-red'])
-<Box bg="light-red" />
+// color
+<Box color='tomato' />
 
-// line-height: 1.5 (theme.lineHeights.copy)
-<Box lineHeight="copy" />
+// color: #333 (theme.colors.gray[0])
+<Box color='grays.0' />
 
-// Получается width: 50% т.к. значение не указано в теме
-<Box width={1 / 2} />
+// background color
+<Box bg='tomato' />
+
+// responsive width
+<Box width={[1, 1 / 2, 1 / 4]} />
+
+// responsive font-size
+<Box fontSize={[2, 3, 4]} />
+
+// responsive margin
+<Box m={[1, 2, 3]} />
+
+// responsive padding
+<Box p={[1, 2, 3]} />
